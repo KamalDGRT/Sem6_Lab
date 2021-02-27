51c40aca002a5e1e8090d0f0db9944d9b3d58fb4
KamalDGRT <kamaldgrt@gmail.com>
2021-02-27 23:43:10 +0530

    added Ex01 Q01 solution01.py

---
+++ Python_and_ML_Lab/Practice_Ex_01/Question_01/solution_02.py
@@ -0,0 +1,11 @@
+'''
+    Question 01:
+    Write code in python program to display the subject 
+    code and name of your lab in two different lines.
+
+    My Solution 2: 
+    This worked for some reason and the test cases got cleared.
+'''
+
+code = input()
+print('Python Programming and Machine Learning Lab')
