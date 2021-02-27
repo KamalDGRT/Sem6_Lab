e6ae7bd42e92fbe7eb2f7bd73efe1cdd6bcb8826
KamalDGRT <kamaldgrt@gmail.com>
2021-02-28 00:07:30 +0530

    added Ex01 Q03 solution01.py

---
+++ Python_and_ML_Lab/Practice_Ex_01/Question_03/solution_01.py
@@ -0,0 +1,36 @@
+'''
+    Question 03:
+    Write code in python to print various datatypes of variable x.
+
+    My Solution: 
+'''
+
+a = "fruits"
+b = 20
+c = 20.5
+d = 1j
+e = ["apple", "orange", "pomegranate"]
+f = ("banana", "strawberry", "grape")
+g = range(8)
+h = {"pomegranate" : "fruit", "class" : "flora" }
+i = {"kiwi", "dragon fruit", "custard apple"}
+j = frozenset({"kiwi", "dragon fruit", "custard apple"})
+k = True
+l = b"Hello"
+m = bytearray(5)
+n = memoryview(bytes(5))
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
