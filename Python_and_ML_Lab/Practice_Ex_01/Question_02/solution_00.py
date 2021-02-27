a41c411c892d077b29f123cd72f5c0cad6160ff8
KamalDGRT <kamaldgrt@gmail.com>
2021-02-27 23:54:24 +0530

    added Ex01 Q02 solution00.py

---
+++ Python_and_ML_Lab/Practice_Ex_01/Question_02/solution_00.py
@@ -0,0 +1,17 @@
+'''
+    Question 02:
+    Write code in python to print addition, 
+    subtraction, multiplication, division, 
+    modulus, exponentiation of any two numbers.
+
+    Question Author's Solution: 
+'''
+
+x = 5
+y = 8
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
