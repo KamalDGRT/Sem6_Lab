b17141aaa745aaacb26d7c81e444b2fdb469945d
Kamal Sharma <kamaldgrt@gmail.com>
2021-02-27 13:14:01 +0530

    Create dateclient.java

---
+++ Networking_Lab/Experiment_02/dateclient.java
@@ -0,0 +1,30 @@
+/*
+    Experiment 02
+    Creation of Date Server
+        - Client Side Program
+*/
+
+import java.io.*;
+import java.net.*;
+
+class dateclient {
+    public static void main(String args[]) {
+
+        Socket soc;
+        DataInputStream dis;
+        String sdate;
+
+        try { 
+            InetAddress ia = InetAddress.getLocalHost(); 
+            soc = new Socket(ia, 8020);
+            dis = new DataInputStream(soc.getInputStream()); 
+            sdate = dis.readLine(); 
+            System.out.println("The data in the server is: " + sdate);
+        } 
+
+        catch (IOException e) { 
+            System.out.println("The exception is: " + e);
+        }
+
+    }
+}
