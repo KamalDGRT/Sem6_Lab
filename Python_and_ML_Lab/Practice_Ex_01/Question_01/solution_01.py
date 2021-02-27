9a24789f2111a4eebf6065e46f6c5a7090bc7290
KamalDGRT <kamaldgrt@gmail.com>
2021-02-27 23:41:20 +0530

    added Ex01 Q01 solution01.py

---
+++ Python_and_ML_Lab/Practice_Ex_01/Question_01/solution_01.py
@@ -0,0 +1,10 @@
+'''
+    Question 01:
+    Write code in python program to display the subject 
+    code and name of your lab in two different lines.
+
+    My Solution 1: 
+'''
+
+print('SCS4305')
+print('Python Programming and Machine Learning Lab')
