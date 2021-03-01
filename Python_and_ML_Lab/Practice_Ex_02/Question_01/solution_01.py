cc1b49acc69d0407c78954e37a05a69edf39553c
Kamal Sharma <kamaldgrt@gmail.com>
2021-03-01 23:47:44 +0530

    added Ex02 Q01 solution_01.py

---
+++ Python_and_ML_Lab/Practice_Ex_02/Question_01/solution_01.py
@@ -0,0 +1,11 @@
+'''
+    Question 01:
+    Write python code to add any two numbers.
+
+    My Solution: 
+'''
+
+a = float(input())
+b = float(input())
+
+print("The sum of " + str(a) + " and " + str(b) + " is " + str(a + b))
