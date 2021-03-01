36f72c3dd6103e2ebf92ee6c1b68134b932b459c
Kamal Sharma <kamaldgrt@gmail.com>
2021-03-01 23:46:39 +0530

    added Ex02 Q01 solution00.py

---
+++ Python_and_ML_Lab/Practice_Ex_02/Question_01/solution_00.py
@@ -0,0 +1,11 @@
+'''
+    Question 01:
+    Write python code to add any two numbers.
+
+    Question Author's Solution: 
+'''
+
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
